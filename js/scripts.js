ea19dc5f73ab7483cba2d0c2c0b1b6b4756dd462
Kira <rosehill1788@protonmail.com>
2019-08-30 12:05:46 -0700

    fix issue with findCost prototype concatenating array elements instead of adding them

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,18 @@ Pizza.prototype.addSize = function(size) {
   this.size = size;
 }
 
-Pizza.prototype.addToppings = function(toppingChoices) {
-  this.toppings.push(toppingChoices);
+Pizza.prototype.addToppings = function(cheese, meat, vegetable, sauce) {
+  this.toppings.push(cheese, meat, vegetable, sauce);
 }
 
 Pizza.prototype.findCost = function() {
   var totalOfToppings = 0;
-  this.toppings.forEach(function(val) {
-    totalOfToppings = totalOfToppings + val;
-  })
-  this.toppings += totalOfToppings;
-  this.total = this.toppings + this.size;
+  this.toppings.forEach(function(i) {
+    totalOfToppings += i;
+  });
+  this.toppings = 0;
+  totalOfToppings += this.toppings;
+  this.total = totalOfToppings + this.size;
 }
 
 //FRONT END
@@ -30,20 +31,26 @@ $(document).ready(function() {
   $("#formPizza").submit(function(event) {
     event.preventDefault();
     var inputSize = parseInt($("#formSize").val());
+    console.log(inputSize);
     var inputCheese = parseInt($("#formCheese").val());
+    console.log(inputCheese);
     var inputMeat = parseInt($("#formMeat").val());
+    console.log(inputMeat);
     var inputVegetable = parseInt($("#formVegetables").val());
+    console.log(inputVegetable);
     var inputSauce = parseInt($("#formSauce").val());
+    console.log(inputSauce);
 
     pizza1 = new Pizza();
-    console.log(pizza1);
+    // console.log(pizza1);
     pizza1.addSize(inputSize);
-    console.log(pizza1.size);
+    // console.log(pizza1.size);
     pizza1.addToppings(inputCheese, inputMeat, inputVegetable, inputSauce);
     console.log(pizza1.toppings);
     var cost = pizza1.findCost();
-    console.log(pizza1.total);
+    // console.log(pizza1.total);
     var finalPrice = pizza1.total;
+    // console.log(finalPrice);
 
 
     $("#displayCost").text(finalPrice)
