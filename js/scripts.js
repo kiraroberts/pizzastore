1ed5054b5b481311854b7c5ab1054998d2b647f6
Kira <rosehill1788@protonmail.com>
2019-08-30 09:47:11 -0700

    add findCost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
 function Pizza() {
   this.size = 0,
-  this.toppings = []
+  this.toppings = [],
+  this.total = 0
 }
 
 Pizza.prototype.addSize = function(size) {
@@ -11,4 +12,13 @@ Pizza.prototype.addSize = function(size) {
 
 Pizza.prototype.addToppings = function(topping) {
   this.toppings.push(topping);
+}
+
+Pizza.prototype.findCost() {
+  var totalOfToppings = 0;
+  this.toppings.forEach(function(val) {
+    totalOfToppings = totalOfToppings + val;
+  });
+  this.toppings += totalOfToppings;
+  this.total = this.toppings + this.size;
 }
